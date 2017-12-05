72d6ae7d7ae4b96ea9989e778320528bcde5b0d5
Cool Dev <cool.webpro17@gmail.com>
2017-12-04 17:11:54 -0800

    remove two tales in models

--- mediacoin/models.py
+++ mediacoin/models.py
@@ -21,41 +21,6 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-# class WalletTransaction(models.Model):
-#     created_at = models.DateTimeField(default=datetime.datetime.now)
-#
-#     from_wallet = models.ForeignKey(
-#         'Wallet',
-#         null=True,
-#         related_name="sent_transactions"
-#     )
-#     to_wallet = models.ForeignKey(
-#         'Wallet',
-#         null=True,
-#         related_name="received_transactions"
-#     )
-#
-#     to_bitcoinaddress = models.CharField(
-#         max_length=50,
-#         blank=True
-#     )
-#
-#
-# class Wallet(models.Model):
-#     created_at = models.DateTimeField(default=datetime.datetime.now)
-#     updated_at = models.DateTimeField()
-#
-#     label = models.CharField(max_length=50, blank=True)
-#
-#     transactions_with = models.ManyToManyField(
-#         'self',
-#         through=WalletTransaction,
-#         symmetrical=False)
-#
-#     transaction_counter = models.IntegerField(default=1)
-#
-#     last_balance = models.DecimalField(default=Decimal(0), max_digits=16, decimal_places=8)
-
 class Transaction(models.Model):
     created_at = models.DateTimeField(default=datetime.datetime.now)
     amount = models.DecimalField(
